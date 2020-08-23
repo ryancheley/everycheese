12b71828428f9da8ef7f058b830e631a9fc039b2
Ryan Cheley <rcheley@gmail.com>
2020-08-23 10:24:16 -0700

    added detail view of cheese

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -5,4 +5,5 @@ app_name = "cheeses"
 
 urlpatterns = [
     path(route='', view=views.CheeseListView.as_view(), name="list"),
+    path(route='<slug:slug>', view=views.CheeseDetailView.as_view(), name="detail"),
 ]
